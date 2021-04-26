cea9b6e3190379f59ac3a805a63c8f46058b3e93
serjio <baghi@yandex.ru>
2021-04-26 23:51:39 +0400

    datecalc & timer built with webpack

--- hometask_2/src/diffDates.js
+++ hometask_2/src/diffDates.js
@@ -1,4 +1,5 @@
-import { DateTime } from './luxon.js';
+// import { DateTime } from './luxon.js';
+import { DateTime } from 'luxon';
 
 export function diffDates(firstDate, secondDate) {
 	firstDate = DateTime.fromISO(firstDate);
